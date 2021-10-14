3fc9fe959e68ba9aaad8901c2d0e523ad5d87341
Tarikh Agustia <agustia.tarikh150@gmail.com>
2021-10-14 09:24:05 +0700

    Update UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,9 +41,9 @@
                 <h1 class="mb-4">Discover cultural diversity in this place</h1>
                 <p class="caps">A landscape is the result of the succession, over time, of the environmental, social, cultural, and economicproductive evolutionary processes that occur in an area</p>
             </div>
-            <a href="https://vimeo.com/45830194" class="icon-video popup-vimeo d-flex align-items-center justify-content-center mb-4">
-                <span class="fa fa-play"></span>
-            </a>
+{{--            <a href="https://vimeo.com/45830194" class="icon-video popup-vimeo d-flex align-items-center justify-content-center mb-4">--}}
+{{--                <span class="fa fa-play"></span>--}}
+{{--            </a>--}}
         </div>
     </div>
 </div>
