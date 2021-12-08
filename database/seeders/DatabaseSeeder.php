3ac5dc545faa5b29b38243d127658c8bfe3d7ae6
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-12-08 22:56:00 +0700

    add `Event` model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        $this->call(UserSeeder::class);
+        $this->call(BookingSeeder::class);
+        $this->call(EventSeeder::class);
     }
 }
