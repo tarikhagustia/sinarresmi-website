08ea3ac151cfdb5d5d9eaba0d2af092798a8b7ab
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-12-10 13:53:59 +0700

    add old input value as value

--- resources/views/dashboard/users/create.blade.php
+++ resources/views/dashboard/users/create.blade.php
@@ -12,7 +12,7 @@
                     <span class="text-gray-700 dark:text-gray-400">
                         Name
                     </span>
-                    <input name="name" type="text"
+                    <input name="name" type="text" value="{{ old('name') }}"
                         class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                         placeholder="Name" />
                     <span class="text-xs text-gray-600 dark:text-gray-400">
@@ -26,7 +26,7 @@
                     <span class="text-gray-700 dark:text-gray-400">
                         Email
                     </span>
-                    <input name="email" type="text"
+                    <input name="email" type="text" value="{{ old('email') }}"
                         class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                         placeholder="Email" />
                     <span class="text-xs text-gray-600 dark:text-gray-400">
