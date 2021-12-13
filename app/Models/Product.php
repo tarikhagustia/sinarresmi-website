e7c1d830cfbd179177d99dae8d2a0e8defa215dd
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-12-13 21:35:50 +0700

    add `serialNumbers` relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function serialNumbers()
+    {
+        return $this->hasMany(SerialNumber::class);
+    }
 }
